885936b927edcbe9c763c1b5168f4606582f780a
Ford Fowler <fordjfowler@gmail.com>
2016-11-09 15:42:10 -0800

    refactor github requests

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express'),
   // NOTE: require in our request proxy module
-  
+  requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
 
 // NOTE: now use our proxy within a function to request
 //        our github data on the server.
-
+var proxyGithub = function(request, response) {
+  console.log('Routing Github request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + requests.params[0],
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
+  }));
+};
 
 app.use(express.static('./'));
 
