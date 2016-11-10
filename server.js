59b00f396016cd6b98515f21da80ee7c596e6a8e
Chaimaebettah <bettah.chaimae@gmail.com>
2016-11-09 16:24:39 -0800

    hhh

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('*', function(request, response) {
   response.sendFile('index.html', { root: '.' });
 });
 
+
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
