b6adf6177de3a6315878654dd6110c51cfdbe03c
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-11-09 16:03:38 -0800

    stuff

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,8 +4,12 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.get call
     $.when(
-     $.get('/github/codefellows-seattle-301d14'),
-     $.get('/github/users/joeyderosa/followers')
+     $.get('/github/users/codefellows-seattle-301d14', function(data) {
+       reposObj.allRepos = data;
+     }),
+     $.get('/github/users/joeyderosa/followers',function(data) {
+       reposObj.followers = data;
+     })
     ).done(callback);
   };
 
