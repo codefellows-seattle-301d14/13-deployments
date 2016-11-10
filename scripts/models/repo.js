3a79ef0567a0c0dda4c459194a75ce1841104e3e
Ben Petty <benjamin.s.petty@gmail.com>
2016-11-09 16:01:08 -0800

    define reposObj and functions

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -4,8 +4,12 @@
   reposObj.requestRepos = function(callback) {
     // NOTE: refactor this request into an $.get call
     $.when(
-    $.get(''),
-    $.get('')
+      $.get('/github/users/benpetty/repos', function(data) {
+        reposObj.allRepos = data;
+      }),
+      $.get('/github/users/benpetty/followers', function(data) {
+        reposObj.follwers = data;
+      })
     ).done(callback);
   };
 
